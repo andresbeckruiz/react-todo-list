8542c769d28fd4268b6cecfe2a38299dcf3f36d6
Andres Beck-Ruiz <andresbeckruiz@Andress-MacBook-Pro.local>
2020-06-18 23:25:01 -0700

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import {v4 as uuidv4} from 'uuid';
 import TodoInput from './components/TodoInput';
 import Orange from './components/TodoList';
-
+// showing vs-code github setup
 uuidv4();
 export default class App extends Component {
   render() {
